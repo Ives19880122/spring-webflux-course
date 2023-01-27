af15df2549007a9f184c1f7e914e33b98d2494e2
Ives <jianxing.imo99g@gmail.com>
2023-01-27 22:41:45 +0800

    feat: 建立dto,service,controller 改寫成Flux與Mono

---
+++ webflux-practice/src/main/java/com/ives/webfluxpractice/controller/ReactiveMathController.java
@@ -0,0 +1,29 @@
+package com.ives.webfluxpractice.controller;
+
+import com.ives.webfluxpractice.dto.Response;
+import com.ives.webfluxpractice.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
+public class ReactiveMathController {
+
+    @Autowired
+    private ReactiveMathService reactiveMathService;
+
+    @GetMapping("square/{input}")
+    public Mono<Response> findSquare(@PathVariable int input){
+        return reactiveMathService.findSquare(input);
+    }
+
+    @GetMapping("table/{input}")
+    public Flux<Response> multiplicationTable(@PathVariable int input){
+        return reactiveMathService.multiplicationTable(input);
+    }
+}
