4d2709b0efa97f4eefdb07a2e72351b513af179f
Ives <jianxing.imo99g@gmail.com>
2023-01-27 22:44:34 +0800

    fix: controller 使用TEXT_EVENT_STREAM_VALUE 回傳資料

--- webflux-practice/src/main/java/com/ives/webfluxpractice/controller/ReactiveMathController.java
+++ webflux-practice/src/main/java/com/ives/webfluxpractice/controller/ReactiveMathController.java
@@ -3,6 +3,7 @@ package com.ives.webfluxpractice.controller;
 import com.ives.webfluxpractice.dto.Response;
 import com.ives.webfluxpractice.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,11 @@ public class ReactiveMathController {
 
     @GetMapping("table/{input}")
     public Flux<Response> multiplicationTable(@PathVariable int input){
+        return reactiveMathService.multiplicationTable(input);
+    }
+
+    @GetMapping(value = "table/{input}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Response> multiplicationTableStream(@PathVariable int input){
         return reactiveMathService.multiplicationTable(input);
     }
 }
