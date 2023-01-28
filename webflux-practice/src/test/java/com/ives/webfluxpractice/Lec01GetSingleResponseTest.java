696374251497c563d5afb5d9f276c6c24be3dbcd
Ives <jianxing.imo99g@gmail.com>
2023-01-28 23:45:46 +0800

    fix: 測試1 使用WebClient進行BlockTest

--- webflux-practice/src/test/java/com/ives/webfluxpractice/Lec01GetSingleResponseTest.java
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec01GetSingleResponseTest.java
@@ -1,5 +1,8 @@
 package com.ives.webfluxpractice;
 
+import com.ives.webfluxpractice.dto.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -9,6 +12,19 @@ public class Lec01GetSingleResponseTest extends BaseTest{
     @Autowired
     private WebClient webClient;
 
+    @Test
+    public void blockTest(){
 
+        Response response = webClient.get()
+                .uri("reactive-math/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .block();
+
+        System.out.println(response);
+
+        Assertions.assertEquals(25,response.getOutput());
+
+    }
 
 }
