1809dcdb0147c9cc141d926878d42fc5486ed2fe
Ives <jianxing.imo99g@gmail.com>
2023-01-28 15:40:15 +0800

    fix: Request Predicates 不同的條件會進入不同的requestHandler
    1. 使用路徑的Regex檢核輸入值，不通過將無法進入router

--- webflux-practice/src/main/java/com/ives/webfluxpractice/config/RouterConfig.java
+++ webflux-practice/src/main/java/com/ives/webfluxpractice/config/RouterConfig.java
@@ -5,10 +5,7 @@ import com.ives.webfluxpractice.exception.InputValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import java.util.function.BiFunction;
@@ -33,7 +30,9 @@ public class RouterConfig {
 
     public RouterFunction<ServerResponse> serverResponseRouterFunction(){
         return RouterFunctions.route()
-                .GET("square/{input}",requestHandler::squareHandler)
+                // 使用路徑的Regex檢核輸入值
+                .GET("square/{input}", RequestPredicates.path("*/1?").or(RequestPredicates.path("*/20")),requestHandler::squareHandler)
+                .GET("square/{input}",req->ServerResponse.badRequest().bodyValue("only 10-20 allow"))
                 .GET("table/{input}",requestHandler::tableHandler)
                 .GET("table/{input}/stream",requestHandler::tableStreamHandler)
                 .POST("multiply",requestHandler::multiplyHandler)
