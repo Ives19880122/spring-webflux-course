49da9e597881279905e6cb103f197eda90fec5b8
Ives <jianxing.imo99g@gmail.com>
2023-01-29 00:05:12 +0800

    fix: 測試1 使用WebClient進行stepVerifierTest
    1. 製作stepVerifierTest方法
    2. 修正pom.xml netty錯誤警告, 追加m1 mac 修補用 dependency

--- webflux-practice/src/test/java/com/ives/webfluxpractice/Lec01GetSingleResponseTest.java
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec01GetSingleResponseTest.java
@@ -1,11 +1,15 @@
 package com.ives.webfluxpractice;
 
 import com.ives.webfluxpractice.dto.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+@Slf4j
 public class Lec01GetSingleResponseTest extends BaseTest{
 
     // 這邊需要建立一個專屬的Bean,故要使用Configuration
@@ -24,6 +28,21 @@ public class Lec01GetSingleResponseTest extends BaseTest{
         System.out.println(response);
 
         Assertions.assertEquals(25,response.getOutput());
+
+    }
+
+    @Test
+    public void stepVerifierTest(){
+
+        Mono<Response> responseMono = webClient.get()
+                .uri("reactive-math/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .log();
+
+        StepVerifier.create(responseMono)
+                .expectNextMatches(val -> val.getOutput() == 25)
+                .verifyComplete();
 
     }
 
