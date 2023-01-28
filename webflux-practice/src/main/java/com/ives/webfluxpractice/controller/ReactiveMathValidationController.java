cb6a7fd97e8e68d2524c4168f2bcf164ccadae8d
Ives <jianxing.imo99g@gmail.com>
2023-01-28 11:39:10 +0800

    feat: Assignment01 不符合條件回傳400, 不用Exception Handler

--- webflux-practice/src/main/java/com/ives/webfluxpractice/controller/ReactiveMathValidationController.java
+++ webflux-practice/src/main/java/com/ives/webfluxpractice/controller/ReactiveMathValidationController.java
@@ -4,6 +4,7 @@ import com.ives.webfluxpractice.dto.Response;
 import com.ives.webfluxpractice.exception.InputValidationException;
 import com.ives.webfluxpractice.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,15 @@ public class ReactiveMathValidationController {
                 })
                 .cast(Integer.class)
                 .flatMap(reactiveMathService::findSquare);
+    }
+
+    @GetMapping("square/{input}/assignment")
+    public Mono<ResponseEntity<Response>> assignment(@PathVariable int input){
+        return Mono.just(input)
+                .filter(i -> i>=10 && i<=20)
+                .flatMap(reactiveMathService::findSquare)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
     }
 
 }
