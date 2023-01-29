26f5efe89366f2b0466ea6f3fd48f998c2dc6fb8
Ives <jianxing.imo99g@gmail.com>
2023-01-29 13:36:24 +0800

    fix: 測試4 headersTest

---
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec04HeadersTest.java
@@ -0,0 +1,38 @@
+package com.ives.webfluxpractice;
+
+import com.ives.webfluxpractice.dto.MultiplyRequestDto;
+import com.ives.webfluxpractice.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec04HeadersTest extends BaseTest{
+
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    public void headersTest(){
+        Mono<Response> responseMono = webClient.post()
+                .uri("reactive-math/multiply")
+                .bodyValue(buildRequestDto(5, 3))
+                .headers(h->h.set("someKey","someVal"))
+                .retrieve()
+                .bodyToMono(Response.class)
+                .log();
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+    }
+
+    private MultiplyRequestDto buildRequestDto(int a, int b){
+        MultiplyRequestDto requestDto = new MultiplyRequestDto();
+        requestDto.setFirst(a);
+        requestDto.setSecond(b);
+        return requestDto;
+    }
+}
