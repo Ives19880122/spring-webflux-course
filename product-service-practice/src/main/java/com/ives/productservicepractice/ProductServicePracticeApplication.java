755c67ffc53cb455e47d1acaf987f67dbde20ade
Ives <jianxing.imo99g@gmail.com>
2023-01-29 20:43:04 +0800

    feat: product-service project setup

---
+++ product-service-practice/src/main/java/com/ives/productservicepractice/ProductServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ives.productservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicePracticeApplication.class, args);
+	}
+
+}
