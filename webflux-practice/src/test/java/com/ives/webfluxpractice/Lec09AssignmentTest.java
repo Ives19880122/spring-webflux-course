9265d9d6a02d80aebc0c8e4e285c361a0fd558bd
Ives <jianxing.imo99g@gmail.com>
2023-01-29 16:46:45 +0800

    fix: 測試9 AssignmentTest參考 Solution改寫
    1. 使用flux range 與 flatMap組合資料
    2. 把WebClient邏輯抽出來共同使用

--- webflux-practice/src/test/java/com/ives/webfluxpractice/Lec09AssignmentTest.java
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec09AssignmentTest.java
@@ -4,78 +4,87 @@ import com.ives.webfluxpractice.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
 
 public class Lec09AssignmentTest extends BaseTest{
 
     @Autowired
     WebClient webClient;
     private static final int FIRST_NUMBER = 10;
-    private static final int[] SECOND_NUMBERS = {1,2,3,4,5};
+    private static final String FORMAT = "%d %s %d = %s";
 
     @Test
     public void plusTest(){
-        for (int val: SECOND_NUMBERS) {
-            Mono<Response> responseMono = webClient.get()
-                    .uri(u -> u.path("calculator/{first}/{second}").build(FIRST_NUMBER, val))
-                    .headers(httpHeaders -> httpHeaders.set("OP","+"))
-                    .retrieve()
-                    .bodyToMono(Response.class)
-                    .log();
+        // 改用Flux range
+        Flux<String> stringFlux = Flux.range(1, 5)
+                .flatMap(v -> send(v, "+"))
+                .log();
 
-            StepVerifier.create(responseMono)
-                    .expectNextCount(1)
-                    .verifyComplete();
-        }
+        StepVerifier.create(stringFlux)
+                .expectNextCount(5)
+                .verifyComplete();
     }
+
 
     @Test
     public void minusTest(){
-        for (int val: SECOND_NUMBERS) {
-            Mono<Response> responseMono = webClient.get()
-                    .uri(u -> u.path("calculator/{first}/{second}").build(FIRST_NUMBER, val))
-                    .headers(httpHeaders -> httpHeaders.set("OP","-"))
-                    .retrieve()
-                    .bodyToMono(Response.class)
-                    .log();
+        Flux<String> stringFlux = Flux.range(1, 5)
+                .flatMap(v -> send(v, "-"))
+                .log();
 
-            StepVerifier.create(responseMono)
-                    .expectNextCount(1)
-                    .verifyComplete();
-        }
+        StepVerifier.create(stringFlux)
+                .expectNextCount(5)
+                .verifyComplete();
     }
 
     @Test
     public void multiplyTest(){
-        for (int val: SECOND_NUMBERS) {
-            Mono<Response> responseMono = webClient.get()
-                    .uri(u -> u.path("calculator/{first}/{second}").build(FIRST_NUMBER, val))
-                    .headers(httpHeaders -> httpHeaders.set("OP","*"))
-                    .retrieve()
-                    .bodyToMono(Response.class)
-                    .log();
+        Flux<String> stringFlux = Flux.range(1, 5)
+                .flatMap(v -> send(v, "*"))
+                .log();
 
-            StepVerifier.create(responseMono)
-                    .expectNextCount(1)
-                    .verifyComplete();
-        }
+        StepVerifier.create(stringFlux)
+                .expectNextCount(5)
+                .verifyComplete();
     }
 
     @Test
     public void divisionTest(){
-        for (int val: SECOND_NUMBERS) {
-            Mono<Response> responseMono = webClient.get()
-                    .uri(u -> u.path("calculator/{first}/{second}").build(FIRST_NUMBER, val))
-                    .headers(httpHeaders -> httpHeaders.set("OP","/"))
-                    .retrieve()
-                    .bodyToMono(Response.class)
-                    .log();
+        Flux<String> stringFlux = Flux.range(1, 5)
+                .flatMap(v -> send(v, "/"))
+                .log();
 
-            StepVerifier.create(responseMono)
-                    .expectNextCount(1)
-                    .verifyComplete();
-        }
+        StepVerifier.create(stringFlux)
+                .expectNextCount(5)
+                .verifyComplete();
     }
 
+    /**
+     * 如果要照著順序顯示可改用flatMapSequential
+     */
+    @Test
+    public void test(){
+        Flux<String> stringFlux = Flux.range(1, 5)
+                .flatMap(v -> Flux
+                        .just("+","-","*","/")
+                        .flatMap(op->send(v,op)))
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(20)
+                .verifyComplete();
+    }
+
+    private Mono<String> send(int sec, String operation) {
+        return webClient.get()
+                .uri(u -> u.path("calculator/{first}/{second}").build(FIRST_NUMBER, sec))
+                .headers(httpHeaders -> httpHeaders.set("OP", operation))
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(result -> String.format(FORMAT, FIRST_NUMBER, operation, sec, result));
+    }
 }
