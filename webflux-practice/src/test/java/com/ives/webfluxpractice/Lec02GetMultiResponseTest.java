eabfc844394210e6674e6de6367ab9f48f8821c7
Ives <jianxing.imo99g@gmail.com>
2023-01-29 11:26:54 +0800

    feat: 測試2 fluxTest

---
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec02GetMultiResponseTest.java
@@ -0,0 +1,30 @@
+package com.ives.webfluxpractice;
+
+import com.ives.webfluxpractice.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class Lec02GetMultiResponseTest extends BaseTest{
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void fluxTest(){
+
+        Flux<Response> responseFlux = webClient.get()
+                .uri("reactive-math/table/{number}", 5)
+                .retrieve()
+                .bodyToFlux(Response.class)
+                .log();
+
+        StepVerifier.create(responseFlux)
+                .expectNextCount(10)
+                .verifyComplete();
+
+    }
+
+}
