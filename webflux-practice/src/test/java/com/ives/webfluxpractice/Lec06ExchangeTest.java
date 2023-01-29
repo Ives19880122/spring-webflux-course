686bf406fdc9371837500a0d7201321e35ed5811
Ives <jianxing.imo99g@gmail.com>
2023-01-29 14:02:09 +0800

    fix: 測試6 exchangeTest

---
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec06ExchangeTest.java
@@ -0,0 +1,41 @@
+package com.ives.webfluxpractice;
+
+import com.ives.webfluxpractice.dto.InputFailedValidationResponse;
+import com.ives.webfluxpractice.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec06ExchangeTest extends BaseTest{
+
+    @Autowired
+    WebClient webClient;
+
+    /**
+     * exchange = retrieve + additional info http status code
+     */
+    @Test
+    public void exchangeTest(){
+        Mono<Object> responseMono = webClient.get()
+                .uri("reactive-math/square/{number}/throw", 5)
+                .exchangeToMono(this::exchange)
+                .doOnNext(System.out::println)
+                .doOnError(err -> System.out.println(err.getMessage()));
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+    }
+
+    private Mono<Object> exchange(ClientResponse cr){
+        if(cr.rawStatusCode() == 400)
+            return cr.bodyToMono(InputFailedValidationResponse.class);
+        else
+            return cr.bodyToMono(Response.class);
+    }
+
+}
