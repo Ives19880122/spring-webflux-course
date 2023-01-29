d56aa1809451bf9ea10a3e663cfeeba8ec29e4ed
Ives <jianxing.imo99g@gmail.com>
2023-01-29 13:50:09 +0800

    fix: 測試5 badRequestTest

---
+++ webflux-practice/src/test/java/com/ives/webfluxpractice/Lec05BadRequestTest.java
@@ -0,0 +1,30 @@
+package com.ives.webfluxpractice;
+
+import com.ives.webfluxpractice.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec05BadRequestTest extends BaseTest{
+
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    public void badRequestTest(){
+        Mono<Response> responseMono = webClient.get()
+                .uri("reactive-math/square/{number}/throw", 5)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .doOnNext(System.out::println)
+                .doOnError(err -> System.out.println(err.getMessage()));
+
+        StepVerifier.create(responseMono)
+                .verifyError(WebClientResponseException.BadRequest.class);
+
+    }
+
+}
