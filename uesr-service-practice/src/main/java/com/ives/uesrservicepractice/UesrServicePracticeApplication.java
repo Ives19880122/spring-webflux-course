baf4723c4fe239612d7c1d7d350a5ba1d0d010f3
Ives <jianxing.imo99g@gmail.com>
2023-02-01 22:05:56 +0800

    feat: 建立User Service Project

---
+++ uesr-service-practice/src/main/java/com/ives/uesrservicepractice/UesrServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ives.uesrservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UesrServicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UesrServicePracticeApplication.class, args);
+	}
+
+}
---
+++ uesr-service-practice/src/test/java/com/ives/uesrservicepractice/UesrServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ives.uesrservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UesrServicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
