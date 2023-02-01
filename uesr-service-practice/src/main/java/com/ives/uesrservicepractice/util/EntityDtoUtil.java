1b20b7f3322c7bcc0ee90796736fcf6c68ee6f5b
Ives <jianxing.imo99g@gmail.com>
2023-02-01 23:52:29 +0800

    fix: EntityDtoUtil 建立Transaction相關轉換Util

--- uesr-service-practice/src/main/java/com/ives/uesrservicepractice/util/EntityDtoUtil.java
+++ uesr-service-practice/src/main/java/com/ives/uesrservicepractice/util/EntityDtoUtil.java
@@ -1,8 +1,14 @@
 package com.ives.uesrservicepractice.util;
 
+import com.ives.uesrservicepractice.dto.TransactionRequestDto;
+import com.ives.uesrservicepractice.dto.TransactionResponseDto;
+import com.ives.uesrservicepractice.dto.TransactionStatus;
 import com.ives.uesrservicepractice.dto.UserDto;
 import com.ives.uesrservicepractice.entity.User;
+import com.ives.uesrservicepractice.entity.UserTransaction;
 import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDateTime;
 
 public class EntityDtoUtil {
 
@@ -16,5 +22,21 @@ public class EntityDtoUtil {
         User user = new User();
         BeanUtils.copyProperties(dto,user);
         return user;
+    }
+
+    public static UserTransaction toEntity(TransactionRequestDto requestDto){
+        UserTransaction ut = new UserTransaction();
+        ut.setUserId(requestDto.getUserId());
+        ut.setAmount(requestDto.getAmount());
+        ut.setTransactionDate(LocalDateTime.now());
+        return ut;
+    }
+
+    public static TransactionResponseDto toDto(TransactionRequestDto requestDto, TransactionStatus status){
+        TransactionResponseDto responseDto = new TransactionResponseDto();
+        responseDto.setAmount(requestDto.getAmount());
+        responseDto.setUserId(requestDto.getUserId());
+        responseDto.setStatus(status);
+        return responseDto;
     }
 }
