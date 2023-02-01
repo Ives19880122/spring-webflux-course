13b53e37aed70838656f7638fb98d4f196119848
Ives <jianxing.imo99g@gmail.com>
2023-02-01 23:47:01 +0800

    fix: UserRepository追加updateUserBalance方法

--- uesr-service-practice/src/main/java/com/ives/uesrservicepractice/repository/UserRepository.java
+++ uesr-service-practice/src/main/java/com/ives/uesrservicepractice/repository/UserRepository.java
@@ -1,9 +1,21 @@
 package com.ives.uesrservicepractice.repository;
 
 import com.ives.uesrservicepractice.entity.User;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User,Integer> {
+
+    @Modifying
+    @Query(
+            "update users " +
+            "set balance = balance - :amount " +
+            "where userId = :userId " +
+            "and balance >= :amount"
+    )
+    Mono<Boolean> updateUserBalance(Integer userId, Integer amount);
 }
