8adf276715edc685341e27e3227b1a78444eff26
Ives <jianxing.imo99g@gmail.com>
2023-02-04 16:22:41 +0800

    feat: setup order-service maven

---
+++ order-service-practice/src/main/java/com/ives/orderservicepractice/OrderServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ives.orderservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServicePracticeApplication.class, args);
+	}
+
+}
---
+++ order-service-practice/src/test/java/com/ives/orderservicepractice/OrderServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ives.orderservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
