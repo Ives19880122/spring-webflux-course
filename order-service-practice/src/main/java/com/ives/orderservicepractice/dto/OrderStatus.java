cd075c503c3447451a9bbc39999f42c07e0f0f24
Ives <jianxing.imo99g@gmail.com>
2023-02-04 20:38:32 +0800

    feat: order-service dto 建立

---
+++ order-service-practice/src/main/java/com/ives/orderservicepractice/dto/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.ives.orderservicepractice.dto;
+
+public enum OrderStatus {
+    COMPLETED,
+    FAILED
+}
---
+++ order-service-practice/src/main/java/com/ives/orderservicepractice/dto/TransactionStatus.java
@@ -0,0 +1,6 @@
+package com.ives.orderservicepractice.dto;
+
+public enum TransactionStatus {
+    APPROVED,
+    DECLINED
+}
