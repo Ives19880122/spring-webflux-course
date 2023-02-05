8a21ecc5ff0680e74bb4bf1cb3a37ea821fcaa39
Ives <jianxing.imo99g@gmail.com>
2023-02-05 15:45:32 +0800

    fix: 調整加入服務錯誤時的回傳處理

--- order-service-practice/src/main/java/com/ives/orderservicepractice/controller/PurchaseController.java
+++ order-service-practice/src/main/java/com/ives/orderservicepractice/controller/PurchaseController.java
@@ -5,7 +5,11 @@ import com.ives.orderservicepractice.dto.PurchaseOrderResponseDto;
 import com.ives.orderservicepractice.service.OrderFulfillmentService;
 import com.ives.orderservicepractice.service.OrderQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,8 +24,11 @@ public class PurchaseController {
     private OrderQueryService queryService;
 
     @PostMapping
-    public Mono<PurchaseOrderResponseDto> order(@RequestBody Mono<PurchaseOrderRequestDto> requestDtoMono){
-        return orderFulfillmentService.processOrder(requestDtoMono);
+    public Mono<ResponseEntity<PurchaseOrderResponseDto>> order(@RequestBody Mono<PurchaseOrderRequestDto> requestDtoMono){
+        return orderFulfillmentService.processOrder(requestDtoMono)
+                .map(ResponseEntity::ok)
+                .onErrorReturn(WebClientResponseException.class,ResponseEntity.badRequest().build())
+                .onErrorReturn(WebClientRequestException.class,ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build());
     }
 
     @GetMapping("user/{userId}")
